c8a6dd856e1bfadab8f2fce883d225e1de387b5f
Accazeta <f.panzeri24@studenti.unibg.it>
2023-04-27 13:57:13 +0200

    Added data structure for clients and addCustomer method

--- Bank/src/bank/Bank.java
+++ Bank/src/bank/Bank.java
@@ -1,12 +1,16 @@
 package bank;
 
+import java.util.ArrayList;
+
 public class Bank implements Visitable{
 	public final String nome;
 	public final double codiceFiscale;
+	private ArrayList<Customer> clients;
 	
 	Bank() {
 		this.nome = "Banca di Bergamo";
 		this.codiceFiscale = (int) ((Math.random() * (9999999 - 1000000)) + 1000000);
+		this.clients = new ArrayList<Customer>();
 	}
 	
 	@Override
@@ -17,6 +21,10 @@ public class Bank implements Visitable{
 	@Override
 	public String toString() {
 		return this.nome;
+	}
+	
+	public void addCustomer(Customer c) {
+		this.clients.add(c);
 	}
 
 }
