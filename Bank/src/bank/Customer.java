60c3df7c700e6440f73ece19ff11253d34ec4988
Accazeta <f.panzeri24@studenti.unibg.it>
2023-04-27 10:57:31 +0200

    Added private field "conti" with getter

--- Bank/src/bank/Customer.java
+++ Bank/src/bank/Customer.java
@@ -1,16 +1,21 @@
 package bank;
 
+import java.util.ArrayList;
+
 public class Customer extends Bank{
 	public static int c_id=0;
 	private final int id;
 	private String name;
 	private String surname;
+	private ArrayList<Account> conti;
 	
 	public Customer() {
 		super();
 		this.name = "";
 		this.surname = "";
 		this.id = ++c_id;
+		this.conti = new ArrayList<Account>();
+		this.conti.add(new Account());
 	}
 	
 	public Customer(String name, String surname) {
@@ -18,6 +23,8 @@ public class Customer extends Bank{
 		this.name = name;
 		this.surname = surname;
 		this.id = ++c_id;
+		this.conti = new ArrayList<Account>();
+		this.conti.add(new Account());
 	}
 	
 	@Override
@@ -43,6 +50,10 @@ public class Customer extends Bank{
 
 	public void setSurname(String surname) {
 		this.surname = surname;
+	}
+	
+	public ArrayList<Account> getConti() {
+		return this.conti;
 	}
 
 	@Override
