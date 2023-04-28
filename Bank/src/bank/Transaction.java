2d9046329df231dc3e014cc939ebac124ca3fa90
Accazeta <f.panzeri24@studenti.unibg.it>
2023-04-28 11:26:39 +0200

    Added Transaction class

--- Bank/src/bank/Transaction.java
+++ Bank/src/bank/Transaction.java
@@ -1,5 +1,18 @@
 package bank;
 
-public class Transaction {
-
+public class Transaction extends Bank{
+	Customer source;
+	Customer destination;
+	Double amount;
+	
+	public Transaction(Customer source, Customer destination, Double amount) {
+		this.source = source;
+		this.destination = destination;
+		this.amount = amount;
+	}
+	
+	@Override
+	public <T> T accept(Visitor<T> v) {
+		return v.visit(this);
+	}
 }
